fcdf77e058a4ca26fc3b985230f8cc2db58ee5ff
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-07-11 10:06:34 -0500

    code to connect to database and migrate the first model

---
+++ initializer/db.go
@@ -0,0 +1,37 @@
+package initializer
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fajardodiaz/infosgroup-employee-management/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func ConnectToDatabase() {
+
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASSWORD")
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	database := os.Getenv("DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, database)
+
+	Db, err := gorm.Open(mysql.New(mysql.Config{
+		DriverName: "mysql",
+		DSN:        dsn,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Migrate the models
+	Db.AutoMigrate(&models.Employee{})
+
+}
