809b93530e141fd9e18b0618264625d84aa00c99
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-07-16 00:18:46 -0500

    post and get controllers ready

--- controllers/genderController.go
+++ controllers/genderController.go
@@ -12,7 +12,9 @@ import (
 
 func GetGendersHandler(w http.ResponseWriter, r *http.Request) {
 	var genders []models.Gender
+
 	initializer.Db.Find(&genders)
+
 	response, err := json.Marshal(&genders)
 	if err != nil {
 		log.Fatal(err)
@@ -23,15 +25,31 @@ func GetGendersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetGenderHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.Gender
 	vars := mux.Vars(r)
-	id := vars["id"]
-	var gender models.Gender
-	initializer.Db.First(&gender, id)
-
+	initializer.Db.Find(&user, vars["id"])
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(&user)
 }
 
 func PostGenderHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("POST"))
+	var gender models.Gender
+	json.NewDecoder(r.Body).Decode(&gender)
+
+	err := gender.Validate()
+	if err != nil {
+		resp := make(map[string]string)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		resp["message"] = err.Error()
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	initializer.Db.Create(&gender)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&gender)
 }
 
 func PutGenderHandler(w http.ResponseWriter, r *http.Request) {
