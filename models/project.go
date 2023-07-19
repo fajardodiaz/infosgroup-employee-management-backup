d0cdb6338e6a54c978f5316b377c9ace0ddca227
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-07-19 11:45:09 -0500

    Added Validate() function

--- models/project.go
+++ models/project.go
@@ -1,8 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Project struct {
 	gorm.Model
 	Name string `gorm:"not null;serializer:json"`
+}
+
+func (p *Project) Validate() error {
+	if p.Name == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
 }
