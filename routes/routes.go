4a32583a0044132d11c750a80b83f61c5793fed1
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-07-18 21:09:04 -0500

    routes with /api/v1 prefix

--- routes/routes.go
+++ routes/routes.go
@@ -8,12 +8,24 @@ import (
 func GetRoutes() *mux.Router {
 	R := mux.NewRouter()
 
-	gender := R.PathPrefix("/gender").Subrouter()
-	gender.HandleFunc("/", controllers.GetGendersHandler).Methods("GET")
+	v1 := R.PathPrefix("/api/v1").Subrouter()
+
+	// Gender routes
+	gender := v1.PathPrefix("/genders").Subrouter()
+
+	gender.HandleFunc("", controllers.GetGendersHandler).Methods("GET")
 	gender.HandleFunc("/{id:[0-9]+}", controllers.GetGenderHandler).Methods("GET")
-	gender.HandleFunc("/", controllers.PostGenderHandler).Methods("POST")
+	gender.HandleFunc("", controllers.PostGenderHandler).Methods("POST")
 	gender.HandleFunc("/{id}", controllers.PutGenderHandler).Methods("PUT")
 	gender.HandleFunc("/{id}", controllers.DeleteGenderHandler).Methods("DELETE")
+
+	// Position routes
+	position := v1.PathPrefix("/positions").Subrouter()
+	position.HandleFunc("", controllers.GetPositionsHandler).Methods("GET")
+	position.HandleFunc("/{id:[0-9]+}", controllers.GetPositionHandler).Methods("GET")
+	position.HandleFunc("", controllers.PostPositionHandler).Methods("POST")
+	position.HandleFunc("/{id:[0-9]+}", controllers.PutPositionHandler).Methods("PUT")
+	position.HandleFunc("/{id:[0-9]+}", controllers.DeletePositionHandler).Methods("DELETE")
 
 	return R
 }
