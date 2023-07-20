ac63f24de29fb8c07c5bebfbaf889dca1b65ec8c
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-07-20 02:28:28 -0500

    Validate() function created

--- models/state.go
+++ models/state.go
@@ -1,8 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type State struct {
 	gorm.Model
 	Name string `gorm:"not null;serializer:json"`
+}
+
+func (s *State) Validate() error {
+	if s.Name == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
 }
--- models/team.go
+++ models/team.go
@@ -1,8 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Team struct {
 	gorm.Model
 	Name string `gorm:"not null;serializer:json"`
+}
+
+func (t *Team) Validate() error {
+	if t.Name == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
 }
